251347fa0816b3207d783db8dd39fb5dbe64fce2
Superr <106885631+SuperKaa@users.noreply.github.com>
2024-11-09 17:13:50 +0000

    Update main.py

--- main.py
+++ main.py
@@ -2,15 +2,14 @@ import random
 import time
 
 print("Very Cool Dice")
-loop = 1
 
 time.sleep(1.5)
 
-while loop == 1:
+while True:
     num = random.randint(1,6)
     print(num)
     q = input("again? y/n ==>")
     if q == "n":
         print("Bye")
-        loop = 2
+        break
     
